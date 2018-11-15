1b0f45de5612186f8373c4973299fdad6b3e8a75
Jared Le Doux <jaredledoux@Jareds-MacBook-Pro.local>
2018-11-14 17:59:24 -0800

    Added music looping in background.

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ var req = new XMLHttpRequest();
 req.open('GET','https://api.punkapi.com/v2/beers?brewed_after=11-2013', true);
 req.send();
 
-
 req.onload = function() {
 //console.log(req);
+
+  var Music = new Tone.Player("99_bottles_of_beer.wav").toMaster();
+  //starts music as soon as the buffer is loaded
+  Music.autostart = true;
+  Music.loop = true;
 
   var response = req.response;
   var beerList = JSON.parse(response);
